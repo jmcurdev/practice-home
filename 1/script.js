425a622db236ee187af07743672acef9cd303018
jmcurdev <jmcur.dev@gmail.com>
2015-11-27 14:06:50 -0300

    Navbar added

--- 1/script.js
+++ 1/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
+
+	$(".well").addClass("animated bounceInUp");
+	$(".navbar").addClass("animated bounceInDown");
+
+	/*$(".navbar-nav li a").hover(function(){
+			$(this).addClass("animated wobble");
+	});*/
+
 	$("#btnNini").click(function(){
-		
 		$("#resultBtnNini").toggle(200);
 		$("#resultBtnNini").css("display", "block");
 		$("#resultBtnNini").text("Holis!");
 	});
 });
-
-
